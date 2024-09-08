a19329c2301a9a4144e9f35ac09800af33ffe5b2
wancup <hebium@wancup.dev>
2024-09-08 21:56:21 +0900

    ➕ setup sass

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import "./app.css";
+import "./app.scss";
 
 export default function App() {
   return (
