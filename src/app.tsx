660371c3608735c28ff8d5484586ec1924b4193f
wancup <hebium@wancup.dev>
2024-09-18 00:11:01 +0900

    ➖ Remove sass

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { type JSX, Suspense } from "solid-js";
-import "./app.scss";
+import "./app.css";
 
 export default function App(): JSX.Element {
   return (
