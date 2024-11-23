e5f6f68fcbdb736993b04d0a71df8778bcfa946d
wancup <hebium@wancup.dev>
2024-11-24 00:19:23 +0900

    ✨ setup favicon

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,6 +9,11 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon-180x180.png" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png" />
           <link rel="icon" href="/favicon.ico" />
           <script
             // eslint-disable-next-line solid/no-innerhtml
