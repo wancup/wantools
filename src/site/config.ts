dace4e27dab6c7907213bdfc3e31432371b1964d
wancup <hebium@wancup.dev>
2024-11-25 21:44:32 +0900

    🔧 Minor change of site description

--- src/site/config.ts
+++ src/site/config.ts
@@ -1,6 +1,6 @@
 export const SITE = {
   name: "wanTooLs",
-  description: "Utility tools for web application developers",
+  description: "Utility tools for web application developers!",
 } as const;
 
 export const COLOR_THEME_OPTION = {
