433ebda1a2c8eefa3259dc2955eec41437f97557
wancup <hebium@wancup.dev>
2025-06-14 13:46:49 +0900

    🏗️ Migrate to devinxi from vinxi

--- app/client.tsx
+++
@@ -1,8 +0,0 @@
-/// <reference types="vinxi/types/client" />
-import { StartClient } from "@tanstack/solid-start";
-import { hydrate } from "solid-js/web";
-import { createRouter } from "./router";
-
-const router = createRouter();
-
-hydrate(() => <StartClient router={router} />, document.body);
